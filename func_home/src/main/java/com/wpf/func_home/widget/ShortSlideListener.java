75042882fbb25dfb0d7d54b339f6206c6caa9b4f
Pengfei Wang <pengfei.wang@anker.com>
2021-12-09 11:37:42 +0800

    添加上啦更多功能测试

---
+++ func_home/src/main/java/com/wpf/func_home/widget/ShortSlideListener.java
@@ -0,0 +1,10 @@
+package com.wpf.func_home.widget;
+
+/**
+ * Author: feipeng.wang
+ * Time:   2021/12/7
+ * Description : This is description.
+ */
+public interface ShortSlideListener {
+    void onShortSlide(float eventY);
+}
